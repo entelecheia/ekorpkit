ef7091b29fc6b2b12e9d4551e3c5c9c854e6e451
Young Joon Lee <yj.lee@chu.ac.kr>
2022-02-26 21:26:40 +0900

    add cd (#3)
    * add automated releases on tags

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set next version number
+RELEASE=$1
+
+# Create tags
+git commit --allow-empty -m "Release $RELEASE"
+git tag -a $RELEASE -m "Version $RELEASE"
+
+# Push
+git push upstream --tags
