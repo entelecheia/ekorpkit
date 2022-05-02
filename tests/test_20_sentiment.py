394a2221b779d50ee15a110d9953d5a624227a13
Young Joon Lee <yj.lee@chu.ac.kr>
2022-05-02 06:36:47 +0000

    feat: add predict sentiments test

--- tests/test_20_sentiment.py
+++ tests/test_20_sentiment.py
@@ -21,22 +21,24 @@ def test_setiment_lexicon():
     sentimetns = lexicon.analyze(tokens, tags=["Positive", "Negative", "Uncertainty"])
     assert len(sentimetns) == len(tokens)
 
+
 def test_predict_sentiments():
-    config_group='model/sentiment=lm'
+    config_group = "model/sentiment=lm"
     model_cfg = eKonf.compose(config_group=config_group)
     model_cfg.preprocessor.tokenizer.nltk.lemmatize = True
 
-    ds_cfg = eKonf.compose(config_group='dataset=dataset')
-    ds_cfg.name = 'financial_phrasebank'
-    ds_cfg.data_dir = './data/tmp'
+    ds_cfg = eKonf.compose(config_group="dataset=dataset")
+    ds_cfg.name = "financial_phrasebank"
+    ds_cfg.data_dir = "./data/tmp"
 
-    cfg = eKonf.compose(config_group='pipeline=pipeline')
+    cfg = eKonf.compose(config_group="pipeline=pipeline")
+    cfg.output_dir = "./data/tmp/predict"
     cfg.dataset = ds_cfg
-    cfg._pipeline_ = ['subset', 'predict']
+    cfg._pipeline_ = ["subset", "predict"]
     cfg.subset.sample_n = 5
     cfg.predict.model = model_cfg
-    cfg.predict.output_file = f'{ds_cfg.name}.parquet'
+    cfg.predict.output_file = f"{ds_cfg.name}.parquet"
     cfg.num_workers = 1
     df = eKonf.instantiate(cfg)
-    
+
     assert len(df) == 15
