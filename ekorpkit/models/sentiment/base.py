2877cf8db3f6fec61379e189ec52d37396e031d3
Young Joon Lee <yj.lee@chu.ac.kr>
2022-06-18 06:53:44 +0000

    feat: support doc level analysis for sentiment analyser

--- ekorpkit/models/sentiment/base.py
+++ ekorpkit/models/sentiment/base.py
@@ -11,9 +11,11 @@ class _Keys(str, Enum):
     DEFAULT = eKonf.Keys.DEFAULT.value
     FEATURE = "feature"
     FEATURES = "features"
+    ARTICLE_FEATURES = "article_features"
     LEXICON_FEATURES = "lexicon_features"
     POLARITY = "polarity"
     NUM_TOKENS = "num_tokens"
+    NUM_EXAMPLES = "num_examples"
     COUNT = "count"
     EVAL = "eval"
     AGG = "agg"
@@ -39,6 +41,7 @@ class SentimentAnalyser:
         self._predict_.features = eKonf.ensure_list(self._predict_.features)
         self._eval_ = args.get(eKonf.Keys.EVAL)
         self._features = args.get(_Keys.FEATURES)
+        self._article_features = args.get(_Keys.ARTICLE_FEATURES)
         self.verbose = args.get("verbose", False)
 
         self._ngram = eKonf.instantiate(args.model.ngram)
@@ -68,31 +71,47 @@ class SentimentAnalyser:
         if isinstance(features, str):
             features = [features]
         tokens = self._ngram.tokenize(text)
+        num_examples = len(tokens)
         _lex_feats = self._predict_.get(_Keys.LEXICON_FEATURES)
         lexicon_features = self._ngram.find_features(tokens, features=_lex_feats)
         scores = {}
         for feather in features:
-            score = self._get_score(tokens, lexicon_features, feature=feather)
+            score = self._get_score(
+                lexicon_features, feature=feather, num_examples=num_examples
+            )
             score = self._assign_class(score, feature=feather)
             scores.update(score)
 
         return scores
 
     def predict_article(self, article, features=["polarity"]):
+        scores = {}
         if article is None:
-            return None
+            return scores
 
-        for sent in article.split(self._sentence_separator):
+        article_scores = {}
+        for sent_no, sent in enumerate(article.split(self._sentence_separator)):
             sent = sent.strip()
             if sent:
-                yield self.predict(sent, features=features)
+                article_scores[sent_no] = self.predict(sent, features=features)
+        for feather in features:
+            score = self._get_score(
+                article_scores, feature=feather, _features=self._article_features
+            )
+            score = self._assign_class(score, feature=feather)
+            scores.update(score)
 
-    def _get_score(self, tokens, lexicon_features, feature="polarity"):
+        return scores
+
+    def _get_score(
+        self, lexicon_features, feature="polarity", num_examples=None, _features=None
+    ):
         """Get score for features.
 
         :returns: int
         """
-        _feature = self._features[feature]
+        _features = _features or self._features
+        _feature = _features[feature]
         _default_feature = self._features[_Keys.DEFAULT]
         _lex_feats = _feature.get(_Keys.LEXICON_FEATURES)
         lxfeat = pd.DataFrame.from_dict(lexicon_features, orient="index")
@@ -100,8 +119,10 @@ class SentimentAnalyser:
         score = {}
         if lxfeat.empty:
             return score
-        num_tokens = len(tokens)
-        score[_Keys.NUM_TOKENS.value] = num_tokens
+        _num_examples = _feature.get(_Keys.NUM_EXAMPLES) or _Keys.NUM_TOKENS.value
+        if num_examples is None:
+            num_examples = lxfeat.shape[0]
+        score[_num_examples] = num_examples
 
         eps = self.EPSILON
         if _Keys.EVAL in _feature:
@@ -119,7 +140,9 @@ class SentimentAnalyser:
             log.info("Evaluating %s", feature)
             print(lxfeat)
         for _name, _expr in _evals.items():
-            lxfeat[_name] = np.where(lxfeat.eval(_expr), lxfeat[_count], 0)
+            lxfeat[_name] = np.where(
+                lxfeat.eval(_expr), lxfeat[_count] if _count else 1, 0
+            )
         lxfeat_agg = lxfeat.agg(_agg)
         lxfeat_agg = pd.DataFrame(lxfeat_agg).T
         _feat_score = lxfeat_agg.eval(_score)
