2f79e01efee585f1088e8c932d5a11582ce9217d
entelecheia <yj.lee@chu.ac.kr>
2022-09-08 08:41:31 +0000

    feat: added and modified docker file and scripts for tests

--- scripts/tests/checkout.sh
+++ scripts/tests/checkout.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# Set next version number
 BRANCH=${1:-dev/0.1.39}
 
 git clone https://github.com/entelecheia/ekorpkit.git --branch $BRANCH --single-branch
--- scripts/tests/run-test.sh
+++ scripts/tests/run-test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+BRANCH=${1:-dev/0.1.39}
+
 rm -rf /workspace/projects/ekorpkit-test
+rm -rf ekorpkit
+
+git clone https://github.com/entelecheia/ekorpkit.git --branch $BRANCH --single-branch
 cd ekorpkit
+
 pip install -r requirements-test.txt
 pip install -U -e .[all]
 pytest
-cd ..
 # ./codecov -t ${CODECOV_TOKEN}
+
+cd ..
