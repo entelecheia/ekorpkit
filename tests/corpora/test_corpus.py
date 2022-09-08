67bf71b4bd134cdb266aa306bd9c820e8284f915
entelecheia <yj.lee@chu.ac.kr>
2022-09-08 03:41:28 +0000

    refactor: changed tests code for docker

--- tests/corpora/test_corpus.py
+++ tests/corpora/test_corpus.py
@@ -14,16 +14,16 @@ def test_dummy_corpus():
 
 def test_build_corpora():
     cfg = eKonf.compose("corpus/builtin=_dummy_fomc_minutes")
-    cfg.data_dir = "./data/tmp/fomc_minutes"
+    # cfg.data_dir = "/workspace/data/tmp/fomc_minutes"
     eKonf.instantiate(cfg)
 
     cfg = eKonf.compose("corpus/builtin=_dummy_bok_minutes")
-    cfg.data_dir = "./data/tmp/bok_minutes"
+    # cfg.data_dir = "./data/tmp/bok_minutes"
     eKonf.instantiate(cfg)
 
     cfg = eKonf.compose("corpus=corpora")
     cfg.name = ["bok_minutes", "fomc_minutes"]
-    cfg.data_dir = "./data/tmp"
+    # cfg.data_dir = "./data/tmp"
     crps = eKonf.instantiate(cfg)
     # crps.concat_corpora()
 
@@ -34,31 +34,31 @@ def test_corpus_task():
     corpus_cfg = eKonf.compose("corpus")
     corpus_cfg.name = "bok_minutes"
     corpus_cfg.auto.merge = True
-    corpus_cfg.data_dir = "./data/tmp"
+    # corpus_cfg.data_dir = "./data/tmp"
 
     cfg = eKonf.compose("pipeline")
     cfg.data.corpus = corpus_cfg
     cfg._pipeline_ = ["filter_query", "save_dataframe"]
     cfg.filter_query.query = "filename in ['BOK_20181130_20181218']"
-    cfg.save_dataframe.output_dir = "./data/tmp"
+    # cfg.save_dataframe.output_dir = "./data/tmp"
     cfg.save_dataframe.output_file = "corpus_filtered.parquet"
     eKonf.instantiate(cfg)
 
-    assert os.path.exists("./data/tmp/corpus_filtered.parquet")
+    assert os.path.exists(cfg.save_dataframe.output_dir + "/" + cfg.save_dataframe.output_file)
 
 
 def test_corpora_task():
     corpus_cfg = eKonf.compose("corpus=corpora")
     corpus_cfg.name = ["bok_minutes", "fomc_minutes"]
     corpus_cfg.auto.merge = True
-    corpus_cfg.data_dir = "./data/tmp"
+    # corpus_cfg.data_dir = "./data/tmp"
 
     cfg = eKonf.compose(config_group="pipeline")
     cfg.data.corpus = corpus_cfg
     cfg._pipeline_ = ["filter_query", "save_dataframe"]
     cfg.filter_query.query = "id == 0"
-    cfg.save_dataframe.output_dir = "./data/tmp"
+    # cfg.save_dataframe.output_dir = "./data/tmp"
     cfg.save_dataframe.output_file = "corpora_filtered.parquet"
     eKonf.instantiate(cfg)
 
-    assert os.path.exists("./data/tmp/corpora_filtered.parquet")
+    assert os.path.exists(cfg.save_dataframe.output_dir + "/" + cfg.save_dataframe.output_file)
--- tests/datasets/test_dataset.py
+++ tests/datasets/test_dataset.py
@@ -6,7 +6,7 @@ from ekorpkit import eKonf
 @pytest.mark.skip(reason="data source not available")
 def test_build_financial_phrasebank():
     cfg = eKonf.compose("dataset/simple=financial_phrasebank")
-    cfg.data_dir = "./data/tmp/financial_phrasebank"
+    # cfg.data_dir = "./data/tmp/financial_phrasebank"
     cfg.io.data_dir = cfg.data_dir
     cfg.io.force.build = True
     cfg.io.force.summarize = True
@@ -17,15 +17,15 @@ def test_build_financial_phrasebank():
 def test_build_datasets():
 
     cfg = eKonf.compose("dataset/simple=sst2")
-    cfg.data_dir = "./data/tmp/sst2"
-    cfg.io.data_dir = cfg.data_dir
+    # cfg.data_dir = "./data/tmp/sst2"
+    # cfg.io.data_dir = cfg.data_dir
     cfg.io.force.build = True
     cfg.io.force.summarize = True
     eKonf.instantiate(cfg)
 
     cfg = eKonf.compose("dataset=datasets")
     cfg.name = ["sst2"]
-    cfg.data_dir = "./data/tmp"
+    # cfg.data_dir = "./data/tmp"
     ds = eKonf.instantiate(cfg)
     ds.persist()
 
@@ -34,7 +34,7 @@ def test_build_datasets():
 
 def test_datafame_pipeline():
     cfg = eKonf.compose("pipeline")
-    cfg.data_dir = "./data/tmp/sst2"
+    cfg.data_dir = "/workspace/data/datasets/simple/sst2"
     cfg.data_file = "sst2-train.parquet"
     cfg._pipeline_ = ["summary_stats"]
     cfg.summary_stats.output_file = "stats.yaml"
