abb1fdf8195b9fd6e36ef5352bdee4c86c46b4f3
TrellixVulnTeam <112716341+TrellixVulnTeam@users.noreply.github.com>
2022-10-23 21:57:11 -0500

    Adding tarfile member sanitization to extractall() (#37)

--- ekorpkit/io/fetch/web.py
+++ ekorpkit/io/fetch/web.py
@@ -136,7 +136,26 @@ def web_download_untar(
         return None
     data_root = os.path.dirname(tar_path)
     with tarfile.open(tar_path) as tar:
-        tar.extractall(data_root)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, data_root)
     if verbose:
         print(f"decompress {tar_path}")
 
@@ -188,7 +207,26 @@ def gdrive_download_untar(
         return None
     data_root = os.path.dirname(local_path)
     with tarfile.open(local_path) as tar:
-        tar.extractall(data_root)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, data_root)
     if verbose:
         print(f"decompress {local_path}")
 
