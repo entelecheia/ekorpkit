067846c27297b21db2b97c0bd511aa16c624c7b2
Young Joon Lee <entelecheia@hotmail.com>
2023-03-02 06:53:51 +0000

    chore: change website to homepage

--- ekorpkit/__init__.py
+++ ekorpkit/__init__.py
@@ -8,6 +8,6 @@ about.name = "ekorpkit"
 about.author = "Young Joon Lee"
 about.description = "ekorpkit provides a flexible interface for NLP and ML research pipelines \
 such as extraction, transformation, tokenization, training, and visualization."
-about.website = "https://entelecheia.cc"
+about.homepage = "https://entelecheia.cc"
 about.version = __version__
 global_config.hyfi_package_config_path = "pkg://ekorpkit.conf"
